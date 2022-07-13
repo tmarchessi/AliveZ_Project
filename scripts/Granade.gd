478f74ed034312c5c6e237df4643b0705e59af01
tmarchessi <tomas.marchessi@ing.uchile.cl>
2022-07-13 12:29:41 -0500

    Revert "Merge remote-tracking branch 'origin/matirama' into tomas_branch"
    This reverts commit b294e469316b0a35a447202748e9935201347d97, reversing
    changes made to adbaa9557c6924e75f13295a12756ddabf8d95da.

--- scripts/Granade.gd
+++ scripts/Granade.gd
@@ -1,24 +1,12 @@
 extends Area2D
 
 var SPEED = 150
-var FRICTION = 0.6
-var STARTPOS = Vector2.ZERO
-var DIRECTION = Vector2.ZERO
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
-	STARTPOS = position
 	
 func _on_body_entered(body: Node):
 	print("F")
 	
 func _physics_process(delta):
-	if SPEED > 0:
-		SPEED -= FRICTION
-		position += SPEED*transform.x*delta
-	else:
-		pass
-	var dist = abs(cartesian2polar(position.x - STARTPOS.x,position.y - STARTPOS.y).x)
-
-func _on_Timer_timeout():
-	queue_free()
+	position += SPEED *transform.x*delta
